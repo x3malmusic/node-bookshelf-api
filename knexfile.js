627cc12afd1260bd73f675e2cbf2f611f5535e08
x3malmusic <x3malmusic@gmail.com>
2020-03-25 14:38:07 +0200

    Fix

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,16 @@
+// user: "developer",
+// password: "new_password",
+
 module.exports = {
   client: "pg",
   connection: {
     host: "127.0.0.1",
-    user: "developer",
-    password: "new_password",
+    user: "postgres",
+    password: "root",
     database: "postgres",
-    charset: "utf8"
+    charset: "utf8",
   },
   migrations: {
-    directory: "./migrations"
-  }
+    directory: "./migrations",
+  },
 };
